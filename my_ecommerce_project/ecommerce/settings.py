840b60f413109249c3bac2ac648656c1a3365e5c
Ankita Shrivastava <ankitashrivastava550@gmail.com>
2026-07-29 17:35:15 +0530

    use sqlite database

--- my_ecommerce_project/ecommerce/settings.py
+++ my_ecommerce_project/ecommerce/settings.py
@@ -74,16 +74,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD':'Ankita05',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
